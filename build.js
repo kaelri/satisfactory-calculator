b9b3520435eed070563333c210316238f3d13570
Michael Engard <michael@engard.me>
2023-01-26 22:49:28 -0500

    solved merging

--- build.js
+++ build.js
@@ -1,99 +1,84 @@
-const fs            = require('fs');
-const YAML          = require('yaml');
-const argv          = require('yargs/yargs')(process.argv.slice(2)).argv;
-const util          = require('util')
-const generateHexID = require('./modules/generate-hex-id');
+const fs       = require('fs');
+const YAML     = require('yaml');
+const argv     = require('yargs/yargs')(process.argv.slice(2)).argv;
+const util     = require('util')
+const createID = require('./modules/generate-hex-id');
 
 function main() {
 
-	const data    = getData();
-	const factory = getFactory();
+	const data = getData();
+	const jobs = getJobs();
 
-	const requirements = {
+	const results = {
 		buildings: [],
-		power:     0,
-		ore:       {},
+		resources: {},
 	}
 
-	const queue = structuredClone(factory);
+	while ( jobs.length ) {
 
-	while ( queue.length ) {
+		const job  = jobs[0];
+		const item = data.map.get( job.item );
 
-		const request = queue[0];
-		const item    = data.map.get( request.item );
-
-		// Ignore raw materials & power for now.
+		// Ignore raw materials (ore & power) for this step.
 		if ( item.type !== 'part' ) {
-			queue.shift();
-			break;
+			jobs.shift();
+			continue;
 		}
-
-		// TODO: figure out how to choose the best formula for items that can be made multiple ways.
-		const formula = item.formulas[0];
-		const output  = formula.output[0];
-
-		let scale = Math.ceil( request.num / output.num );
-
+		
 		// Add the building required for this formula and record the total output needed.
-		let building = data.map.get( formula.building );
+		const building = data.map.get( item.formula.building );
 
-		buildingInstance = {
-			id: building.name + '-' + generateHexID(6),
-			name: building.name,
-			product: item.name,
-			formula: formula,
+		buildingConfig = {
+			jobIDs: [ job.jobID ],
+			building: building.name,
+			formula: item.formula,
 			destinations: [
 				{
-					id: request.destination,
-					outputRequested: request.num,
+					jobID: job.parentID,
+					num:   job.num,
 				}
-			],
-			totalRequested: request.num,
-			scale: scale,
-			outputPerInstance: output.num,
-			outputTotal: output.num * scale,
-			surplus: Math.max( 0, ( output.num * scale ) - request.num ),
-			powerPerInstance: building.power,
-			powerTotal: building.power * scale,
+			]
 		}
 
-		requirements.buildings.push( buildingInstance );
+		results.buildings.push( buildingConfig );
 
-		// Add required components to the queue, where they will be processed recursively.
-		formula.input.forEach( input => queue.push({
+		// Add required components to the jobs queue, where they will be processed recursively.
+		buildingConfig.formula.inputs.forEach( input => jobs.push({
+			jobID: createID(6),
+			parentID: job.jobID,
 			item: input.item,
-			num: input.num * scale,
-			destination: buildingInstance.id,
+			num: 0, // default; will be updated in the next loop when real production values are calculated.
 		}));
 
-		queue.shift();
-
+		jobs.shift();
+		
 	}
 
-	// TODO: optimize buildings. If the same combination of building + product has been requested for multiple jobs, and their collective surplus is greater than the output of one instance of their formula, then we can reduce the number of instances until the surplus drops below that threshold. This can cascade down the supply chain, too, so I need to think about how to traverse the list, merge like buildings and reduce their output.
+	results.buildings = mergeBuildingConfigs( results.buildings );
+	results.buildings = populateBuildingConfigs( results.buildings );
 
 	// Get raw materials & power requirements from generated list of required buildings.
-	requirements.buildings.forEach( buildingInstance => {
+	results.buildings.forEach( buildingConfig => {
 
-		requirements.power += buildingInstance.powerTotal;
+		const building = data.map.get( buildingConfig.building );
 
-		buildingInstance.formula.input.forEach( input => {
+		buildingConfig.formula.inputs.forEach( input => {
 
 			const item = data.map.get( input.item );
-			if ( item.type !== 'ore' ) return;
+			if ( item.type === 'part' ) return;
 
-			if ( !requirements.ore[ item.name ] ) {
-				requirements.ore[ item.name ] = 0;
+			if ( !results.resources[ item.name ] ) {
+				results.resources[ item.name ] = 0;
 			}
 
-			requirements.ore[ item.name ] += input.num * buildingInstance.scale;
+			results.resources[ item.name ] += input.num * buildingConfig.scale;
 
 		});
 
 	});
 
 	// TODO: Pretty up the display.
-	logAll( requirements );
+	logAll( results );
 
 }
 
@@ -104,24 +89,47 @@ function getData() {
 	data.map = new Map( data.entities.map( entity => [ entity.name, entity ] ) );
 
 	// Derive additional item info based on building info.
-	// TODO: handle components with multiple formulas. For now, we assume each item has one and only one production method.
 	data.entities.filter( entity => ( entity.type === 'building' ) ).forEach( building => {
+
+		// Translate readable YAML into more formal key-value objects.
+		building.formulas = building.formulas.map( formula => {
+
+			formula.inputs = formula.inputs.map( input => {
+				const itemName = Object.keys(input)[0];
+				return {
+					item: itemName,
+					num: input[ itemName ],
+				}
+			});
+
+			formula.inputs.push({
+				item: 'power',
+				num: building.power
+			});
+	
+			// TODO: handle case where one formula outputs multiple items. This will be unfathomably annoying.
+			formula.outputs = formula.outputs.reduce( (_, output) => {
+				const itemName = Object.keys(output)[0];
+				return {
+					item: itemName,
+					num: output[ itemName ],
+				}
+			}, null );
+
+			return formula;
+	
+		});
 
 		building.formulas.forEach( buildingFormula => {
 
-			if ( !buildingFormula.output.length ) return;
-
-			// TODO: handle case where a formula outputs multiple items. That would be unfathomably annoying.
-			const output   = buildingFormula.output[0];
-			const itemName = output.item;
+			if ( !buildingFormula.outputs.item ) return;
+			
+			const itemName = buildingFormula.outputs.item;
 			const item     = data.map.get( itemName );
 
-			const itemFormula = structuredClone( buildingFormula );
-
-			itemFormula.building = building.name;
-
-			if ( !item.formulas ) item.formulas = [];
-			item.formulas.push( itemFormula );
+			// TODO: conversely, figure out how to choose the best formula for items that can be made multiple ways. For now, we assume each item has one and only one production method.
+			item.formula          = structuredClone( buildingFormula );
+			item.formula.building = building.name;
 
 		});
 
@@ -131,24 +139,83 @@ function getData() {
 
 }
 
-function getFactory() {
+function getJobs() {
 
 	if ( !argv._.length ) throw Error('no-factory');
 
-	const factory = [];
+	const jobs = [];
 
 	const [ factoryID ] = argv._;
 
 	const factoryData = YAML.parse( fs.readFileSync( `./factories/${factoryID}.yml`, 'utf8' ) );
 
-	Object.keys( factoryData ).forEach( itemName => factory.push({
-		item: itemName,
-		num: factoryData[itemName],
-		destination: 'final',
+	Object.keys( factoryData ).forEach( itemName => jobs.push({
+		jobID:    createID(6),
+		parentID: null,
+		item:     itemName,
+		num:      factoryData[ itemName ],
 	}));
 
-	return factory;
+	return jobs;
 
+}
+
+function mergeBuildingConfigs( buildingConfigs ) {
+
+	const buildingConfigsMerged = [];
+
+	buildingConfigs.forEach( buildingConfig => {
+
+		const existingConfig = buildingConfigsMerged.find( maybeConfig => ( buildingConfig.building === maybeConfig.building && buildingConfig.formula.outputs.item === maybeConfig.formula.outputs.item ) );
+
+		if ( existingConfig ) {
+
+			existingConfig.jobIDs       = existingConfig.jobIDs.concat( buildingConfig.jobIDs );
+			existingConfig.destinations = existingConfig.destinations.concat( buildingConfig.destinations );
+
+		} else {
+
+			buildingConfigsMerged.push( buildingConfig );
+
+		}
+
+	});
+
+	return buildingConfigsMerged;
+
+}
+
+function populateBuildingConfigs( buildingConfigs ) {
+
+	buildingConfigs.forEach( buildingConfig => {
+
+		buildingConfig.destinations.forEach( destination => {
+
+			if ( !destination.jobID ) return;
+
+			destination.num = 0;
+
+			const parentConfigs = buildingConfigs.filter( parent => parent.jobIDs.indexOf( destination.jobID ) !== -1 );
+			parentConfigs.forEach( parentConfig => {
+				const parentInput  = parentConfig.formula.inputs.find( input => input.item === buildingConfig.formula.outputs.item );
+				destination.num += parentConfig.scale * parentInput.num;
+			})
+
+		});
+
+		buildingConfig.outputMinimum = buildingConfig.destinations.reduce( (outputTotal, destination) => outputTotal + destination.num, 0 );
+		buildingConfig.scale         = Math.ceil( buildingConfig.outputMinimum / buildingConfig.formula.outputs.num );
+		buildingConfig.outputTotal   = buildingConfig.scale * buildingConfig.formula.outputs.num;
+		buildingConfig.surplus       = Math.max( 0, buildingConfig.outputTotal - buildingConfig.outputMinimum );
+
+	});
+
+	return buildingConfigs;
+
+}
+
+function logAll( target ) {
+	console.log(util.inspect(target, {showHidden: false, depth: null, colors: true}))
 }
 
 try {
@@ -160,8 +227,4 @@ try {
 		default:
 			throw e;
 	}
-}
-
-function logAll( target ) {
-	console.log(util.inspect(target, {showHidden: false, depth: null, colors: true}))
 }
